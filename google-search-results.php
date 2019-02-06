27d69c4c7273ef1380dd8c40c27c717980eaf003
dgburton@me.com <dgburton@me.com>
2019-02-06 17:30:28 +0000

    v0.0.1

---
+++ google-search-results.php
@@ -0,0 +1,88 @@
+<?php
+
+require 'restclient.php';
+
+// SerpWowException
+class SerpWowException extends Exception {}
+
+/*
+  Provides access to Google Search Results via the SerpWow API
+  Get a free API key at https://serpwow.com/
+*/
+class GoogleSearchResults {
+  public $options;
+  public $api;
+  public $api_key;
+  
+  public function __construct($api_key="demo") {
+    $this->api_key = $api_key;
+  }
+  
+  public function json($q) {
+    return $this->search('json', 'json', $q, '/live/search');
+  }
+
+  public function html($q) {
+    return $this->search('string', 'html', $q, '/live/search');
+  }
+
+  public function csv($q) {
+    return $this->search('string', 'csv', $q, '/live/search');
+  }
+
+  public function locations($q) {
+    return $this->search('json', 'json', $q, '/live/locations');
+  }
+
+  public function account() {
+    return $this->search('json', 'json', $q, '/live/account');
+  }
+    
+  function search($decode_format, $output, $q, $path) {
+    if($this->api_key == NULL) {
+      throw new SerpWowException("api_key must be defined in the constructor");
+    }
+    
+    $args = [
+      'base_url' => "https://api.serpwow.com",
+      'format' => $decode_format
+    ];
+    if (isset($output) && ($output != 'json')) {
+      $args['output'] = $output;
+    }
+    $api = new RestClient($args);
+
+    $default_q = [
+      'source' => 'php',
+      'api_key' => $this->api_key
+    ];
+    if ($output != 'json') {
+      $default_q['output'] = $output;
+    }
+    if (isset($q)) {
+      $q = array_merge($default_q, $q);
+    } else {
+      $q = $default_q;
+    }
+
+    $result = $api->get($path, $q);
+
+    if($result->info->http_code == 200 || $result->info->http_code == 301 || $result->info->http_code == 302) {
+      if($decode_format == 'string') {
+        return $result->response;
+      } else {
+        return $result->decode_response();
+      }
+    } else {
+      $error = $result->decode_response();
+      $msg = $error->request_info->message;
+      throw new SerpWowException($msg);
+      return;
+    }
+    
+    throw new SerpWowException("Unexpected failure, please contact hello@serpwow.com: $result");
+    return;
+  }
+  
+}
+?>
